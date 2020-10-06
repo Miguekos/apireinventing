3ad673f03edd7935c19f56e73443cea4e8530526
Miguel Rodriguez <miguekos1233@gmail.com>
2020-10-05 22:13:54 -0500

    upload

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,26 +4,34 @@ const BD = require("../database/pg/postgres");
 module.exports = async (app) => {
     app.post("/api/login", async (req, res, next) => {
 
-        var email = req.body.email,
-            password = req.body.password;
+        const username = req.body.username;
+        const password = req.body.password;
 
-        const query = `SELECT * FROM public.users where email = '${email}'`;
+        const query = `SELECT * from fwconacc.tbusuari where no_usuari = '${username}'`;
         const user = await BD.storePostgresql(query);
         if (!user) {
             res.json({ res: "ko", message: "El usuario no se ha identificado" }).status(404)
         } else {
-            bcrypt.compare(password, user.password, (err, result) => {
-                if (result == true) {
-                    req.session.userID = user.id
-                    req.session.username = user.username
-                    req.session.email = user.email
-                    req.session.logged = true
-                    delete user.password
-                    res.json({ res: 'ok', message: "Wellcome", user }).status(200)
-                } else {
-                    res.json({ res: 'ko', message: "Password Incorrecto", user, result, err }).status(400)
-                }
-            })
+            if (password == user.pw_usuari) {
+                // req.session.userID = user.id
+                // req.session.username = user.username
+                // req.session.logged = true
+                delete user.pw_usuari
+                res.json({ res: 'ok', message: "Wellcome", user }).status(200)
+            } else {
+                res.json({ res: 'ko', message: "Password Incorrecto" }).status(400)
+            }
+            // bcrypt.compare(password, user.password, (err, result) => {
+            //     if (result == true) {
+            //         req.session.userID = user.id
+            //         req.session.username = user.username
+            //         req.session.logged = true
+            //         delete user.password
+            //         res.json({ res: 'ok', message: "Wellcome", user }).status(200)
+            //     } else {
+            //         res.json({ res: 'ko', message: "Password Incorrecto", user, result, err }).status(400)
+            //     }
+            // })
         }
     })
 
@@ -35,10 +43,10 @@ module.exports = async (app) => {
 
         req.session.userID = null
         req.session.username = null
-        req.session.email = null
         req.session.logged = false
         res.json({ res: 'ok', message: "Session cerrada correctamente" }).status(200)
 
     })
 
 }
+
