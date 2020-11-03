b53a089a6240b15b962fa562e675218b147cb839
Miguel Rodriguez <miguekos1233@gmail.com>
2020-11-02 22:40:58 -0500

    update

--- api/routes/users.js
+++ api/routes/users.js
@@ -30,6 +30,26 @@ module.exports = async (app) => {
 
     })
 
+    app.get("/api/v1.0/users/:cousuari", async (req, res, next) => {
+        try {
+            let query;
+            const doc_ide = req.params.cousuari;
+            query = `select * from fwconacc.sp_mostrar_usuari('') where co_usuari = ${doc_ide}`;
+            bitacora.control(query, req.url)
+            const user = await BD.storePostgresql(query);
+            // con esto muestro msj
+            if (user.codRes != 99) {
+                // con esto muestro msj
+                res.json({ res: 'ok', message: "Success", user }).status(200)
+            } else {
+                res.json({ res: 'ko', message: "Error en la query", user }).status(500)
+            }
+        } catch (error) {
+            res.json({ res: 'ko', message: "Error controlado", error }).status(500)
+        }
+
+    })
+
     app.post("/api/v1.0/usuario/:tipo", async (req, res, next) => {
         try {
             const tipo = req.params
