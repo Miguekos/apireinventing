6d162210a0590ae32114e7aa8fd4a52fd5b170c8
Miguekos <miguekos1233@gmail.com>
2021-02-09 16:39:13 -0500

    no_respon

--- api/routes/comerc.js
+++ api/routes/comerc.js
@@ -171,7 +171,7 @@ module.exports = async (app) => {
                     query = `select 
                         co_landin, no_tiplan, fe_regist, no_tipdoc, co_docide, no_apepat, 
                         no_apemat, no_nombre, fe_nacimi, no_estciv, nu_telefo, no_liccon, 
-                        de_experi, no_depart, no_provin, no_distri, no_estado
+                        de_experi, no_depart, no_provin, no_distri, no_estado, no_respon
                     from recomerc.fb_listar_landin(
                         '${fe_regdes}',
                         '${fe_reghas}',
@@ -182,7 +182,7 @@ module.exports = async (app) => {
                     query = `select 
                         co_landin, no_tiplan, fe_regist, no_tipnac, no_tipdoc, 
                         co_docide, no_apepat, no_apemat, no_nombre, fe_nacimi, 
-                        nu_telefo, no_liccon, no_correo, no_estado
+                        nu_telefo, no_liccon, no_correo, no_estado, no_respon
                     from recomerc.fb_listar_landin(
                         '${fe_regdes}',
                         '${fe_reghas}',
@@ -193,7 +193,7 @@ module.exports = async (app) => {
                     query = `select 
                         co_landin, no_tiplan, fe_regist, co_docide, no_apepat,
                         no_apemat, no_nombre, no_tipdoc, fe_nacimi, nu_telefo, 
-                        co_plaveh, no_tipcom, no_vehper, no_estado
+                        co_plaveh, no_tipcom, no_vehper, no_estado, no_respon
                     from recomerc.fb_listar_landin(
                         '${fe_regdes}',
                         '${fe_reghas}',
